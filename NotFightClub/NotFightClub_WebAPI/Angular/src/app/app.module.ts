7ed29f2ede71549f6ba7ea7ed1023aee5206cd6f
Jeffrey Wright <jeffrey.wright@revature.net>
2021-09-23 19:18:39 -0400

    Added the store page

--- NotFightClub/NotFightClub_WebAPI/Angular/src/app/app.module.ts
+++ NotFightClub/NotFightClub_WebAPI/Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
