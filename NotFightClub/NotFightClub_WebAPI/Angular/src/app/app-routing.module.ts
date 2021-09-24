5a5f15582355017c7311658350088d51d77a2815
Simran MAnandhar <simran.manandhar@revature.net>
2021-09-24 16:48:07 -0400

    edit profile components

--- NotFightClub/NotFightClub_WebAPI/Angular/src/app/app-routing.module.ts
+++ NotFightClub/NotFightClub_WebAPI/Angular/src/app/app-routing.module.ts
@@ -2,12 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+<<<<<<< Updated upstream
 
 const routes: Routes = [
   {
     path: 'my-profile',
     component: UserProfileComponent
   }
+=======
+import { ChatComponent } from './chat/chat.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TestComponent } from './test/test.component';
+import { AuthGuard } from './guards/auth.guard';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'test1', component: TestComponent, canActivate:[AuthGuard]},
+  { path: 'chat', component: ChatComponent },
+  { path: 'my-profile', component: UserProfileComponent },
+  { path: 'edit-profile', component: EditProfileComponent}
+>>>>>>> Stashed changes
 ]
 
 
--- NotFightClub/NotFightClub_WebAPI/Angular/src/app/app.module.ts
+++ NotFightClub/NotFightClub_WebAPI/Angular/src/app/app.module.ts
@@ -4,11 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+<<<<<<< Updated upstream
+=======
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule, NgForm } from '@angular/forms';
+import { TestComponent } from './test/test.component';
+import { CommonModule } from '@angular/common';
+import { ChatComponent } from './chat/chat.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+
+
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     UserProfileComponent
+=======
+    StoreComponent,
+    UserProfileComponent,
+    ChatComponent,
+    LoginComponent,
+    RegisterComponent,
+    TestComponent,
+    EditProfileComponent,
+
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
