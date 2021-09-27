719e56bc8e800260ee7d09bfb3a505b359baa6ba
briana.wynn@revature.net <briana.wynn@revature.net>
2021-09-27 14:36:05 -0700

    create character repository and controller, still has bugs when trying to insert into the db

---
+++ NotFightClub/NotFightClub_Logic/Repositiories/CharacterRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotFightClub_Data;
+using NotFightClub_Logic.Interfaces;
+using NotFightClub_Models.Models;
+using NotFightClub_Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotFightClub_Logic.Repositiories
+{
+    public class CharacterRepository: IRepository<ViewCharacter>
+    {
+        private readonly P2_NotFightClubContext _dbContext = new P2_NotFightClubContext();
+        private readonly IMapper<Character, ViewCharacter> _mapper;
+
+        public CharacterRepository(IMapper<Character, ViewCharacter> mapper)
+        {
+            _mapper = mapper;
+        }
+        public async Task<ViewCharacter> Add(ViewCharacter viewCharacter)
+        {
+            Character character = _mapper.ViewModelToModel(viewCharacter);
+            //add to the db
+            //_dbContext.Database.ExecuteSqlInterpolated($"Insert into Character(name, baseform, traitId, DOB) values({user.UserName},{user.Pword},{user.Email},{user.Dob})");
+            _dbContext.Add(character);
+            //save changes
+            _dbContext.SaveChanges();
+            //read user back from the db
+            Character createdCharacter = await _dbContext.Characters.FromSqlInterpolated($"select * from Characters where UserId = {character.UserId}").FirstOrDefaultAsync();
+
+            return _mapper.ModelToViewModel(createdCharacter);
+        }
+    }
+}
---
+++ NotFightClub/NotFightClub_WebAPI/Controllers/CharacterController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotFightClub_Logic.Interfaces;
+using NotFightClub_Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NotFightClub_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private readonly IRepository<ViewCharacter> _cr;
+
+        public CharacterController(IRepository<ViewCharacter> cr)
+        {
+            _cr = cr;
+           
+        }
+
+        // GET: api/<CharacterController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CharacterController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CharacterController>
+        [HttpPost("/[action]")]
+        public async Task<ActionResult<ViewCharacter>> Post([FromBody] ViewCharacter viewCharacter)
+        {
+            if (!ModelState.IsValid) return BadRequest("Invalid data.");
+            //call to repository to add user
+            //return the result
+            //Console.WriteLine(viewUser);
+            var createdCharacter = await _cr.Add(viewCharacter);
+            return Ok(createdCharacter);
+        }
+        // PUT api/<CharacterController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CharacterController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
