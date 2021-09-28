1139a90b6679703e40db71e62bec9bd1e86472f5
zachary woolfolk <zachary.woolfolk@revature.net>
2021-09-28 14:45:26 -0500

    basically functional timer

---
+++ NotFightClub/NotFightClub_Logic/Repositiories/FightRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NotFightClub_Logic.Interfaces;
+using NotFightClub_Models.ViewModels;
+using NotFightClub_Models.Models;
+
+namespace NotFightClub_Logic.Repositiories
+{
+  class FightRepository : IRepository<ViewFight>
+  {
+    public Task<ViewFight> Add(ViewFight obj)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
