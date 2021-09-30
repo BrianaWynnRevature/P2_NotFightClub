4e212ac211d1f8ff118802f02cacfcbe2f306a52
Bitazee <katalenasm2@mailbox.winthrop.edu>
2021-09-30 12:03:29 -0400

    Removed Duplicated function

--- NotFightClub/NotFightClub_Logic/Mappers/FightMapper.cs
+++ NotFightClub/NotFightClub_Logic/Mappers/FightMapper.cs
@@ -25,10 +25,7 @@ namespace NotFightClub_Logic.Mappers
       return viewFight;
     }
 
-    public List<ViewFight> ModelToViewModel(List<Fight> obj)
-    {
-      throw new NotImplementedException();
-    }
+ 
 
     public Fight ViewModelToModel(ViewFight obj)
     {
