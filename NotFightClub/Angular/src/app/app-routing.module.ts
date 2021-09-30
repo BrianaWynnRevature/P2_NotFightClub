ae6a453ea635543296ed0021847b2b13ba4de405
Simran MAnandhar <simran.manandhar@revature.net>
2021-09-30 15:33:24 -0400

    removed test

--- NotFightClub/Angular/src/app/app-routing.module.ts
+++ NotFightClub/Angular/src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './user/user-profile/user-profile.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { TestComponent } from './test/test.component';
-import { AuthGuard } from './guards/auth.guard';
 import { ChatComponent } from './chat/chat.component';
 import { EditProfileComponent } from './user/edit-profile/edit-profile.component';
 import { StoreComponent } from './store/store.component';
@@ -23,7 +21,6 @@ import { FightComponent } from './fight/fight.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'test1', component: TestComponent, canActivate:[AuthGuard]},
   { path: 'chat', component: ChatComponent },
   { path: 'store', component: StoreComponent },
   { path: 'edit-profile/:userId', component: EditProfileComponent }, 
